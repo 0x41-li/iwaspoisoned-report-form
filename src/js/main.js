8e8846ab6c47264177866ccce9dbc5ccb603f614
0x41-li <90697156+0x41-li@users.noreply.github.com>
2023-05-11 20:49:46 +0100

    First commit, report form 80% completed

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// Import our custom CSS
+// import Swiper JS
+import Swiper from "swiper";
+
+// import Swiper styles
+import "swiper/css";
+
+//
+import "../scss/custom.scss";
+
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 4,
+  slidesPerView: "auto",
+});
